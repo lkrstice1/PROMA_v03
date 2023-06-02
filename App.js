1721396e14b6e1e92b51abd71a42a747003c9187
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-06-02 23:50:35 +0200

    Jednostavana react native aplikacija

--- App.js
+++ App.js
@@ -1,19 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
-  const [natpis, postaviNatpis] = useState("Programiranje mobilnih aplikacija")
-  const klik = () => {
-    postaviNatpis('Novi natpis');
+  const [unos, promjenaUnosa] = useState("")
+  const [ciljevi, postaviCiljeve] = useState([])
+  
+  const noviUnos = (tekst) => {
+    promjenaUnosa(tekst);
   }
 
+  const noviCilj = () => {
+    console.log(unos)
+    postaviCiljeve(ciljevi => [...ciljevi, unos])
+    promjenaUnosa('')
+  }
+
+  const stilovi = StyleSheet.create({
+    ekran: {
+      padding: 50
+    },
+    unos: {
+      width: '70%', 
+      marginBottom: 5, 
+      borderBottomColor: 'black', 
+      borderBottomWidth: 1
+    },
+    lista: {
+      padding: 10,
+      marginVertical: 10,
+      backgroundColor: "#ccc",
+      borderColor: 'black',
+      borderWidth: 1
+    }
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>{natpis}</Text>
-      <Button title='Promjena' onPress={klik} />
-      <StatusBar style="auto" />
+    <View style={stilovi.ekran}>
+      <View
+        style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}
+      >
+        <TextInput 
+          placeholder = 'dodaj cilj'
+          style = {stilovi.unos}
+          value={unos}
+          onChangeText={noviUnos}
+        />
+        <Button title='Dodaj' 
+        onPress={noviCilj} />
+      </View>
+      <View>
+        {ciljevi.map(cilj => 
+          <View key={cilj} style={stilovi.lista}>
+            <Text>{cilj}</Text>
+          </View>
+        )}
+      </View>
+      <ScrollView>
+          {ciljevi.map(cilj =>
+            <View key={cilj} style={stilovi.lista}>
+                <Text>{cilj}</Text>
+            </View>
+            )}
+      </ScrollView>
     </View>
+    
   );
 }
 
